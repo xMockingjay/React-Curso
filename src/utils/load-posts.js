0e89ebc3dd7d36c04973f5310da1143187e866db
xMockingjay <aramburocarol@gmail.com>
2022-08-04 10:29:50 -0300

    feat: function to fetch data

---
+++ src/utils/load-posts.js
@@ -0,0 +1,17 @@
+export async function loadPosts() {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all(
+      [postsResponse, photosResponse]
+    );
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((posts, index) => {
+      return { ...posts, cover: photosJson[index].url }
+    });
+
+    return postsAndPhotos;
+}
